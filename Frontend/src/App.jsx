44cf77cd1ed1fd6f70b16e53652a7377522f7532
rk123 <rk6683950@gmail.com>
2026-02-16 17:31:25 +0530

    added navbar section

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import Navbar from './components/components_lite/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      
+  </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/components_lite/Navbar.jsx
@@ -0,0 +1,78 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { User2 } from "lucide-react";
+import { LogOut } from "lucide-react";
+
+const Navbar = () => {
+  const user = false;
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job <span className="text-[#022bf8]">Portal</span>
+          </h1>
+        </div>
+        <div className="flex items-center gap-10">
+          <ul className="flex font-medium items-center gap-6">
+            <li>Home</li>
+            <li>Browse</li>
+            <li>Jobs</li>
+          </ul>
+          {!user ? (
+            <div className="flex items-center gap-2">
+              <Button variant="outline">Login</Button>
+              <Button className="bg-red-600 hover:bg-red-700">Register</Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/evilrabbit.png"
+                    alt="@evilrabbit"
+                  />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="flex items-center gap-4 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src="https://github.com/evilrabbit.png"
+                      alt="@evilrabbit"
+                    />
+                  </Avatar>
+                  <div>
+                    <h3 className="font-medium">Rajnish kumar</h3>
+                    <p className="text-sm text-muted-foreground">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col mt-2 text-gray-600 ">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2></User2>
+                    <Button variant="link">Profile</Button>
+                  </div>
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut></LogOut>
+                    <Button variant="link">Logout</Button>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
