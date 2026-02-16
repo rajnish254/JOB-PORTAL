741b779a9e310c1e79002e9bdc87f9f68c568aff
rk123 <rk6683950@gmail.com>
2026-02-16 21:58:36 +0530

    added navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Button } from './components/ui/button'
-import Navbar from './components/components_lite/Navbar'
+import React from "react";
+import Navbar from "./components/components_lite/Navbar";
+import { createBrowserRouter, Route } from "react-router-dom";
+import { Login } from "./components/authentication/Login.jsx";
+import { Register } from "./components/authentication/Signup.jsx";
+import { Home } from "lucide-react";
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  { path: "/signup", element: <Signup /> },
+]);
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
-
-      
-  </div>
-  )
+      <RouterProvider router={appRouter}></RouterProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
