f475028ae7e31a8f000a282a6036970db691e24c
rk123 <rk6683950@gmail.com>
2026-02-23 17:37:50 +0530

    added cors

--- Backend/index.js
+++ Backend/index.js
@@ -12,16 +12,18 @@ import applicationRoute from "./routes/application.route.js";
 dotenv.config();
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-const corsOptions = {
-  origin: "http://localhost:5173", // Replace with your frontend URL
-  credentials: true,
-};
-app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5001;
 
